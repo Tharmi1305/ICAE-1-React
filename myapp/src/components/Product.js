c74f233456600bc290c90de508584e442cb05c9f
Tharmi1305 <139675003+Tharmi1305@users.noreply.github.com>
2024-12-03 17:26:12 +0000

    Product.js file modified

--- myapp/src/components/Product.js
+++ myapp/src/components/Product.js
@@ -1,18 +1,19 @@
 import '../assets/CSS/layout.css';
-export default function Product(){
+export default function Product({flower,AddToCart}){
     
     return(
         <div className="grid-item">
 
             <div class="card">
-                <img  />
+                <img src={require(`../assets/image/${flower.img}`)} alt={flower.name} />
                 <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
+                    <h5 class="card-title">{flower.name}</h5>
+                    <p>Price: ${flower.price}</p>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
+                        <input type="number" id="quantity" name="quantity" onChange={(e)=>(flower.qty=+e.target.value)} />
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button" onClick={()=>AddToCart(flower)}>Add to Cart</button>
                 </div>
             </div>
         </div>
