c956f2ae637b09d15fc2fe2e2ca8010b6d1356d3
Tharmi1305 <139675003+Tharmi1305@users.noreply.github.com>
2024-12-03 17:28:00 +0000

    Products.js file modified

--- myapp/src/components/Products.js
+++ myapp/src/components/Products.js
@@ -1,25 +1,26 @@
 import '../assets/CSS/layout.css';
 import Product from './Product';
 import Cart from './Cart';
-import {flowers} from './FlowerDB'
+import { flowers } from './FlowerDB';
 import {useState} from 'react'
 
+
 export default function Products(){
-   const[cart,setCart]=useState([]);
+     const [cart,setCart]=useState([]);
 
-   const AddToCart =(flower) =>{
-    const existingItem = cart.find((item) => item.id === flower.id)
-    if(existingItem){
-        setCart(
-            cart.map((item) =>
-                item.id === flower.id ? {...item,qty: item.qty + flower.qty} : item
-            )
-        );
-    }else{
-        setCart([...cart,{...flower}])
-    }
- }
-
+     const AddToCart =(flower) =>{
+        const existingItem = cart.find((item) => item.id === flower.id)
+        if(existingItem){
+            setCart(
+                cart.map((item) =>
+                    item.id === flower.id ? {...item,qty: item.qty + flower.qty} : item
+                )
+            );
+        }else{
+            setCart([...cart,{...flower}])
+        }
+     }
+   
     return(
         <>
             <div className="item1">
@@ -29,19 +30,21 @@ export default function Products(){
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
                     {
-                        //product
                         flowers.map((flower)=>(
-                            <Product key={flower.id} flower={flower}/>
-                        ))
+                            <Product 
+                               key={flower.id}
+                               flower={flower}
+                               onAddToCart={AddToCart}
+                               />
+
+                        )
+                    )
                     }
                 </div>
 
             </div>
             <div className="item3">
-                {
-                //cart
                 <Cart cart={cart}/>
-                }
             </div>
         </>
     );
